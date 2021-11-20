6bc36d380f9a204db979032c07a7f7a7a38c3ab2
Felix-Voyle <felix.voyle@icloud.com>
2021-11-20 08:59:37 +0000

    updated procfile

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
-from lib import app 
+from lib import app
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
